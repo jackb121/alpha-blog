850b990a5421678f9db0e619b5cb1e683a771e9c
Jack Bailin <jackbailin121@gmail.com>
2016-08-31 17:13:26 +0000

    Add admin requirement to create categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -13,7 +13,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do
-     get new_category_path
+    get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
       post categories_path, category: {name: " "}
